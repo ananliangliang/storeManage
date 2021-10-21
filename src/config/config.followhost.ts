a10946292448dafdd54b5e6192456c33736095cf
LiChaoYang <249313573@qq.com>
2021-10-21 16:15:24 +0800

    fix: followhost baseUrl 没有携带协议的bug

--- src/config/config.followhost.ts
+++ src/config/config.followhost.ts
@@ -1,6 +1,6 @@
 export default {
   // baseUrl: '//tools.zjspoint.com',
-  baseUrl: window.location.host,
+  baseUrl: `//${window.location.host}`,
   prefix: '/warehouse',
 
   // baseUrl: "http://192.168.0.121:8082/devops",
